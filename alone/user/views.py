0deda0c92a33a535168c9b256db0ddcb7b309b0b
wang dong <wangdong@t2cloud.net>
2017-12-06 16:49:07 +0800

    Added logout api

--- alone/user/views.py
+++ alone/user/views.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 
 from django.contrib.auth import get_user_model
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from rest_framework import viewsets
 from rest_framework import status
 from rest_framework.decorators import list_route
@@ -23,10 +23,14 @@ class UserViewSet(viewsets.ModelViewSet):
     def login(self, request):
         data = request.data
         check_body_keys(data, ['name', 'password'])
-
         user = authenticate(**data)
         if user is not None:
             login(request, user)
             return empty_response()
         else:
             return error_response('Login failed!', status=status.HTTP_401_UNAUTHORIZED)
+
+    @list_route(methods=['delete'])
+    def logout(self, request):
+        logout(request)
+        return empty_response()
