ea97118635d3c31bfe0972d857d33b149bc94dea
wang dong <wangdong@t2cloud.net>
2018-07-17 10:37:06 +0800

    Add local docker-compose

--- config/settings/local.py
+++ config/settings/local.py
@@ -58,10 +58,9 @@ ELASTICSEARCH_DSL = {
 
 
 RAVEN_CONFIG = {
-    'dsn': 'http://74417134beee46819996a5a52763abe7:bede877d315e4a68864d8a8cb81fdcad@localhost:9000/2',
-    # If you are using git, you can also automatically configure the
-    # release based on the git info.
-    'release': raven.fetch_git_sha(os.path.dirname(BASE_DIR)),
+    'dsn': os.getenv('RAVEN_DSN',
+                     'http://74417134beee46819996a5a52763abe7:bede877d315e4a68864d8a8cb81fdcad@localhost:9000/2'),
+    # 'release': raven.fetch_git_sha(os.path.dirname(BASE_DIR)),
 }
 
 
